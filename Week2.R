32b9ffc92c77b6d53b650ca5f93811b1fa2f56c7
Dewansh  Matharoo <dewanshmatharoo@campus-040-021.ucdavis.edu>
2024-10-03 15:00:09 -0700

    Week 2

--- Week2.R
+++ Week2.R
@@ -1,3 +1,106 @@
-x = 1
+3 + 4 * 123848
 
-x == 3
+4 + (8 * 3) ^ 2
+
+#order of operation test
+
+exp (12)
+
+?log
+
+rm(x)
+
+x <- 1
+
+x
+
+rm(x)
+
+sqrt(exp(4))
+
+mynumber <- 48
+
+mynumber == 3
+
+mynumber - 3
+
+mynumber != 23
+
+mynumber == 23
+
+mynumber != 21
+
+othernumber = -53
+
+mynumber - othernumber
+
+myNumber = 12
+
+log(12)
+
+log("true")
+
+elephant1_kg <- 3492
+elephant2_lb <- 7757
+ls
+
+elephant1_lb <- elephant1_kg * 2.2 
+
+myelephants <- c(elephant1_lb, elephant2_lb)
+
+which(myelephants == max(myelephants))
+
+getwd()
+
+d <- read.csv("./data/tail_length.csv")
+
+dir.create("./lectures")
+
+weight_g <- c(50,60,78,32)
+weight_one_value <- c(50)
+
+rm(weight_one_values)
+
+animals <- c("rat", "mouse", "dog", "boar")
+animals 
+
+length(weight_g)
+
+class(elephant1_lb)
+typeof(animals)
+
+num_char <- c("23", "dog", "FALSE")
+class(num_char)
+
+num_logical <- c("23", "54", "TRUE")
+class(num_logical)
+class(weight_one_value)
+num_logical <- TRUE
+class(num_logical)
+
+num_logical <- c(1, 2, 3, TRUE)
+char_logical <- c("a", "b", "c", TRUE)
+combined_logical <- c(num_logical, char_logical)
+class(combined_logical)
+
+num_logical <- c(23, 54, "TRUE")
+class(num_logical)
+
+num_char <- c(1, 2, 3, "a")
+typeof(num_char)
+
+animals
+animals[2]
+animals[c(2,4)]
+
+weight_g
+weight_g>50
+weight_g[weight_g>20]
+#this is being studied as conditional subsetting but it comes under the broader act of indexing
+
+animals %in% c("rat", "cat", "dog", "goat", "boar", "cat")
+animals
+
+# %in% can be thought of a bucket that R is looking for things in
+# == on the other hand is a command that is trying to match it word to word with the existing animal vector)
+
