d3210c637e1c8a429b3e0dcf1e4c06f3ea2a576d
Dewansh  Matharoo <dewanshmatharoo@campus-022-209.ucdavis.edu>
2024-10-31 13:10:15 -0700

    update

--- Week3.R
+++ Week3.R
@@ -1,3 +1,39 @@
+#week3
+
+#cbind allows us to view various created. vectors in a tabular format.
+
+c("a","b","c","d")
+list("a","b","c","d")
+
+heights <- c(4, 83, 2, 91, NA, 12)
+heights
+
+sum(heights)
+
+#note how with an NA in the vector, R can't find the sum of the heights. To do so, we need to remove NA from that equation.
+
+sum(heights, na.rm = T)
+
+#and then it works! What the above is doing basically is it's making it so that NA removes NA if it sets to TRUE or T, because the defualt vaulue is F   
+
+#there is another way to remove na values from the vector itself
+
+is.na(heights)
+#[1] FALSE FALSE FALSE FALSE  TRUE FALSE
+!is.na(heights)
+#[1]  TRUE  TRUE  TRUE  TRUE FALSE  TRUE
+
+#the above is the way to check if the vector has NAs
+
+heights[!is.na(heights)]
+
+#and that removes NAs from height
+
+#complete.cases is also a way to completely return vectors that have no NA values
+
+heights[complete.cases(heights)]
+
+
 #lists - multiple vectors in one
 
 c(4, 6, "dog")
@@ -5,9 +41,12 @@ list (4, 6, "dog")
 
 a <- list(4, 6, "dog")
 
+b <- list(4, letters, "dog")
+str(b) 
+
+
 #data.frames
 
-letters
 
 data.frame(letters)
 
@@ -17,8 +56,27 @@ dim(df) #rows, columns
 nrow(df)
 ncol(df)
 
+df2 <- data.frame(letters, letters)
+
+df3 <- data.frame(1:26, letters, "dog, cat")
+df3
+
+#how do we make it so that the data frame replicates characters?
+
+#matrices need to be the same kinds of data (all numerical or all characters)
+
+matrix(nrow=10, ncol=10)
+matrix(1:10, nrow=10, ncol=10)
+
 #a matrix is two dimensional (x and y) 
 #it's always good to check how a matrix is going to be presented before letting the matrix be presented
+#note how in the matrix function we ran above, the rows are just 1 value across all the columns. To fix that, we should use the byrow command
+
+matrix(1:10, nrow=10, ncol=10, byrow=T)
+
+#this, however, the columns are now just 1 number. How do we fix this?
+
+matrix(1:10, nrow)
 
 #factors - if there is a level associated with a string of values
 
